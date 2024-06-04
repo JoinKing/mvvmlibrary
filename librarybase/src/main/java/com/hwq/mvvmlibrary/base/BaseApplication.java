5feb4550057d7b23428bc07c8feb03f43eb50e6d
huangwenqiang <viphwq@163.com>
2024-06-04 11:12:24 +0800

    32

--- librarybase/src/main/java/com/hwq/mvvmlibrary/base/BaseApplication.java
+++ librarybase/src/main/java/com/hwq/mvvmlibrary/base/BaseApplication.java
@@ -15,12 +15,6 @@ import com.hwq.mvvmlibrary.utils.Utils;
 public class BaseApplication extends Application {
     private static Application sInstance;
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        setApplication(this);
-    }
-
     /**
      * 当主工程没有继承BaseApplication时，可以使用setApplication方法初始化BaseApplication
      *
