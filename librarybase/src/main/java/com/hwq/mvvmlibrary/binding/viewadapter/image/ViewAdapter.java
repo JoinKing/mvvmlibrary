838477d941f221e7e6accb96a4c8f1080c9450db
huangwenqiang <viphwq@163.com>
2024-06-09 19:48:38 +0800

    1.0.33

--- librarybase/src/main/java/com/hwq/mvvmlibrary/binding/viewadapter/image/ViewAdapter.java
+++ librarybase/src/main/java/com/hwq/mvvmlibrary/binding/viewadapter/image/ViewAdapter.java
@@ -23,21 +23,17 @@ import com.hwq.mvvmlibrary.widget.GlideRoundTransform;
  * Created by king on 2018.12.21
  */
 public final class ViewAdapter {
-    @BindingAdapter(value = {"url","uri", "placeholderRes"}, requireAll = false)
+    @BindingAdapter(value = {"url", "uri", "placeholderRes"}, requireAll = false)
     public static void setImageUri(ImageView imageView, String url, Uri uri, int placeholderRes) {
         if (!TextUtils.isEmpty(url)) {
             //使用Glide框架加载图片
-            RequestOptions options = new RequestOptions()
-                    .centerCrop()
-                    .placeholder(placeholderRes) //预加载图片
+            RequestOptions options = new RequestOptions().centerCrop().placeholder(placeholderRes) //预加载图片
                     .error(placeholderRes) //加载失败图片
                     .priority(Priority.HIGH) //优先级
                     .diskCacheStrategy(DiskCacheStrategy.ALL); //缓存
             Glide.with(imageView.getContext()).load(url).apply(options).into(imageView);
-        }else if(null!=uri){
-            RequestOptions options = new RequestOptions()
-                    .centerCrop()
-                    .placeholder(placeholderRes) //预加载图片
+        } else if (null != uri) {
+            RequestOptions options = new RequestOptions().centerCrop().placeholder(placeholderRes) //预加载图片
                     .error(placeholderRes) //加载失败图片
                     .priority(Priority.HIGH) //优先级
                     .diskCacheStrategy(DiskCacheStrategy.ALL); //缓存
@@ -53,36 +49,31 @@ public final class ViewAdapter {
     @BindingAdapter(value = {"urlRadius", "placeholderRes"}, requireAll = false)
     public static void urlRadius(ImageView imageView, String url, int placeholderRes) {
         if (!TextUtils.isEmpty(url)) {
-            RequestOptions options = new RequestOptions()
-                    .centerCrop()
-                    .placeholder(placeholderRes) //预加载图片
+            RequestOptions options = new RequestOptions().centerCrop().placeholder(placeholderRes) //预加载图片
                     .error(placeholderRes) //加载失败图片
                     .priority(Priority.HIGH) //优先级
                     .diskCacheStrategy(DiskCacheStrategy.ALL) //缓存
                     .transform(new CircleTransformation());
             Glide.with(imageView.getContext()).load(url).apply(options).into(imageView);
-
         }
     }
-    @BindingAdapter(value = {"urlWithRadius", "placeholderRes","glideRadius"}, requireAll = false)
-    public static void urlRadius(ImageView imageView, String url, int placeholderRes,int glideRadius) {
-        if (!TextUtils.isEmpty(url)) {
-            RequestOptions options = new RequestOptions()
-                    .centerCrop()
-                    .placeholder(placeholderRes) //预加载图片
-                    .error(placeholderRes) //加载失败图片
-                    .priority(Priority.HIGH) //优先级
-                    .diskCacheStrategy(DiskCacheStrategy.ALL) //缓存
-                    .transform(new RoundedCorners(glideRadius)); //圆角
-            Glide.with(imageView.getContext()).load(url).apply(options).into(imageView);
 
-        }
+    @BindingAdapter(value = {"urlWithRadius", "placeholderRes", "glideRadius"}, requireAll = false)
+    public static void urlRadius(ImageView imageView, String url, int placeholderRes, int glideRadius) {
+        url = url == null ? "" : url;
+        RequestOptions options = new RequestOptions().centerCrop().placeholder(placeholderRes) //预加载图片
+                .error(placeholderRes) //加载失败图片
+                .priority(Priority.HIGH) //优先级
+                .diskCacheStrategy(DiskCacheStrategy.ALL) //缓存
+                .transform(new RoundedCorners(glideRadius)); //圆角
+        Glide.with(imageView.getContext()).load(url).apply(options).into(imageView);
     }
+
     @BindingAdapter(value = {"drawable"}, requireAll = false)
     public static void setDrawable(View imageView, int url) {
-        if(url!=0){
+        if (url != 0) {
             imageView.setBackground(imageView.getContext().getResources().getDrawable(url));
-        }else {
+        } else {
             imageView.setBackground(null);
         }
     }
